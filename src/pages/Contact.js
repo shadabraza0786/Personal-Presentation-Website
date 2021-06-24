311d3c3795a809be1a488fa09963d8dc76370522
Shadab Raza <shadabraza500@gmail.com>
2021-06-24 23:37:42 +0530

    Changes in Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,8 +4,8 @@ function Contact() {
     return (
         <div>
             <h1>Contact Us</h1>
-            <h4>Mobile No: 9458691938</h4>
-            <h4>Email id: shadabraza500@gmail.com</h4>
+            <h4>Mobile No: 9876543210</h4>
+            <h4>Email id: shadabr@exapmle.com</h4>
             <h4>Address: Noida Sector 58</h4>
             <h4>Pin code: 201301</h4>
             <h4>State:Uttar Pradesh</h4>
