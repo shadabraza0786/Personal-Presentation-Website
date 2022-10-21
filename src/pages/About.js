08145ad341db21737e9626e9c2f06481fb8dd79b
Shadab Raza <shadabraza500@gmail.com>
2022-10-21 13:17:26 +0530

    Updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,11 @@
-import ShadabImg from "../assets/Shadab.jpg";
+import ShadabImg from "../assets/ShadabRaza.jpg";
 
 function About() {
     return (
         <div>
             <h1>About Us</h1>
             <img className="pic" src= {ShadabImg} alt="Loading Animation" />
-            <p className="para1" >Hi! My Name is Shadab Raza, I am a Software Engineer (React Js). Good Knowledge of Html, Css, JavaScript, and React js.Working at Chetu India Pvt.Ltd (Noida). I am a Web Developer and have good knowledge of git and jeera Sofware.</p>
+            <p className="para1" >Hi ! My Name is Shadab Raza, I am a Software Engineer (React Js Developer). I have Good Knowledge of Html, Css, JavaScript, React, Redux and have good knowledge of git and jeera Sofware. Working in Chetu India Pvt.Ltd (Noida.UP).</p>
         </div>
     )
 }
